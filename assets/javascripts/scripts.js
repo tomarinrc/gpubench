d09c4fedaa2d57b1af4c70cfc90843d21371c2f5
tomarinrc <tarce@ibg.local>
2016-08-16 16:04:09 -0500

    Hero Semi-Complete!
    +Hero Added
    +Begining of Charts Added
    
    TODO
    -Finish Chart Labeling

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -3,63 +3,27 @@ $(function() {
 	//Headroom (Animated Header Library)
 	$(".navbar-default").headroom();
 
-
-
-	//Sidebar Accordion
-	/*$('li.sidebar-azlinks-link').on('click', function() {
-		if($('ul.sidebar-azsubmenu').is(':visible')) {
-			$('ul.sidebar-azsubmenu').slideUp(300);
+	//Chartist Initiation
+	new Chartist.Bar('.gpu-chart-1', {
+		labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+		series: [
+			[1400000, 1400000, 1400000, 1400000],
+			[200000, 400000, 500000, 300000],
+			[1400000, 200000, 400000, 600000]
+		]
+	}, {
+		horizontalBars: true,
+		stackBars: true,
+		axisX: {
+			labelInterpolationFnc: function(value) {
+				return(value / 1000) + 'k';
+			}
 		}
-		$(this).next().slideToggle(300);
-	});*/
-	//Navbar Searchbar Expanding
-	/*var submitIcon = $('.searchbox-icon');
-	var inputBox = $('.searchbox-input');
-	var searchBox = $('.searchbox');
-	var isOpen = false;
-	submitIcon.click(function() {
-		if(isOpen == false) {
-			searchBox.addClass('searchbox-open');
-			inputBox.focus();
-			isOpen = true;
-		} else {
-			searchBox.removeClass('searchbox-open');
-			inputBox.focusout();
-			isOpen = false;
+	}).on('draw', function(data) {
+		if(data.type === 'bar') {
+			data.element.attr({
+				style: 'stroke-width: 3.8rem'
+			});
 		}
 	});
-	submitIcon.mouseup(function() {
-		return false;
-	});
-	searchBox.mouseup(function() {
-		return false;
-	});
-	$(document).mouseup(function() {
-		if(isOpen == true) {
-			$('.searchbox-icon').css('display', 'block');
-			submitIcon.click();
-		}
-	});
-	function buttonUp() {
-		var inputVal = $('.searchbox-input').val();
-		inputVal = $.trim(inputVal).length;
-		if(inputVal !== 0) {
-			$('.searchbox-icon').css('display', 'none');
-		} else {
-			$('.searchbox-input').val('');
-			$('.searchbox-icon').css('display', 'block');
-		}
-	}*/
-	//Scroll Bar
-	/*var lastScrollTop = 0;
-	$(window).scroll(function(event) {
-		var st = $(this).scrollTop();
-		if(st > lastScrollTop) {
-			$(".navbar-row-main").slideUp();
-		} else {
-			$(".navbar-row-main").slideDown();
-		}
-		lastScrollTop = st;
-	});*/
-	//End Document Ready (Jquery)
 });
