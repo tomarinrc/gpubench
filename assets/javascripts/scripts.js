0017e0f61c462d26ae743a6ed7e34ab39a01aeac
tomarinrc <tarce@ibg.local>
2016-08-17 16:35:09 -0500

    Chart Structure Added
    Charts DOM Structure has begun.
    Ditching JS charts, going with good ol' html and
    DIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIVSS
    
    -Removed JS Chart libraries
    +Added Chart Structure

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -7,23 +7,33 @@ $(function() {
 	new Chartist.Bar('.gpu-chart-1', {
 		labels: ['Q1', 'Q2', 'Q3', 'Q4'],
 		series: [
-			[1400000, 1400000, 1400000, 1400000],
-			[200000, 400000, 500000, 300000],
-			[1400000, 200000, 400000, 600000]
+			[14000, 12000, 10000, 10000]
 		]
 	}, {
 		horizontalBars: true,
 		stackBars: true,
-		axisX: {
-			labelInterpolationFnc: function(value) {
-				return(value / 1000) + 'k';
-			}
-		}
+		axisY: {
+			onlyInteger: true
+		},
+		plugins: [
+			Chartist.plugins.ctBarLabels({
+				//position: {
+				//	x: function(data) {
+				//		return "50%"
+				//	}
+				//},
+				labelOffset: {
+					y: 5
+				}
+			})
+		]
 	}).on('draw', function(data) {
 		if(data.type === 'bar') {
 			data.element.attr({
-				style: 'stroke-width: 3.8rem'
+				style: 'stroke-width: 38px'
 			});
 		}
 	});
+
+	//End of Document Ready
 });
