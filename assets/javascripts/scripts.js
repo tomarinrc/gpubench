58bff961326fe3e7851e65c7ac7007b03971b136
tomarinrc <tarce@ibg.local>
2016-08-29 16:34:38 -0500

    Specs and Reviews Semi-Complete
    -Still need to work on nav bar scroll spy

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,39 +1,7 @@
 //Start Document Ready (Jquery)
 $(function() {
+	$('main').scrollspy({target: ".navbar-link-section"});
 	//Headroom (Animated Header Library)
 	$(".navbar-default").headroom();
-
-	//Chartist Initiation
-	new Chartist.Bar('.gpu-chart-1', {
-		labels: ['Q1', 'Q2', 'Q3', 'Q4'],
-		series: [
-			[14000, 12000, 10000, 10000]
-		]
-	}, {
-		horizontalBars: true,
-		stackBars: true,
-		axisY: {
-			onlyInteger: true
-		},
-		plugins: [
-			Chartist.plugins.ctBarLabels({
-				//position: {
-				//	x: function(data) {
-				//		return "50%"
-				//	}
-				//},
-				labelOffset: {
-					y: 5
-				}
-			})
-		]
-	}).on('draw', function(data) {
-		if(data.type === 'bar') {
-			data.element.attr({
-				style: 'stroke-width: 38px'
-			});
-		}
-	});
-
 	//End of Document Ready
 });
