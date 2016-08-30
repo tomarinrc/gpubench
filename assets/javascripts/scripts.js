68c7bf4238f1d51ec3ea59d7c170084420ea47db
tomarinrc <tarce@ibg.local>
2016-08-30 16:02:48 -0500

    Scroll Spy Fixed (Eyyy) and New Header Design
    -Scroll Spy Fixed
    -Header Updated to a much more aesthetically apleasing look
    -Various Mobile Design Fixes
    
    Cool Beans

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,6 +1,5 @@
 //Start Document Ready (Jquery)
 $(function() {
-	$('main').scrollspy({target: ".navbar-link-section"});
 	//Headroom (Animated Header Library)
 	$(".navbar-default").headroom();
 	//End of Document Ready
