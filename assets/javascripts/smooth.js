9587c82bd6c8456c44a1850d8e7bc7aebbae8c93
tomarinrc <tarce@ibg.local>
2016-08-31 16:29:14 -0500

    Added 2 Column Structure to Footer

--- assets/javascripts/smooth.js
+++ assets/javascripts/smooth.js
@@ -5,7 +5,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top - 64
+          scrollTop: target.offset().top - 69
         }, 400);
         return false;
       }
